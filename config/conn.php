d813376b0f11019f603df681fae998cd5bee69bb
rahimah28 <91002689+rahimah28@users.noreply.github.com>
2021-09-19 14:46:44 +0800

    Add files via upload

---
+++ config/conn.php
@@ -0,0 +1,14 @@
+<?php
+
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $namadatabase = "alsp";
+
+    $db = mysqli_connect($server, $user, $password, $namadatabase);
+
+    if (!$db) {
+        die("Gagal terhubung ke database: " . mysqli_connect_error());
+    }
+
+?>
